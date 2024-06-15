325e4bad5c685016925cb5196b2961524a398807
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-06-15 23:47:00 +0000

    .exe, not .zip

--- scripts/build-exe.sh
+++ scripts/build-exe.sh
@@ -5,4 +5,3 @@ cd "$(dirname $0)/../"
 rm -f *.exe *.exe.zip
 npx -y esbuild bin/siemens-memory-dumper.js --bundle --outfile=./siemens-memory-dumper.js --format=cjs --platform=node --loader:.node=file --external:@serialport/bindings-cpp
 npx -y pkg -c ./pkg.json ./siemens-memory-dumper.js
-zip siemens-memory-dumper.exe.zip siemens-memory-dumper.exe
